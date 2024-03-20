4cb03b073175a13b7675063d44a67e79b1c07d23
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2024-03-20 21:45:47 +0300

    remove settings

--- app/(auth)/settings/page.tsx
+++ app/(auth)/settings/page.tsx
@@ -1,13 +1,14 @@
-import { SettingsForm } from "@/components/auth/settings-form";
-import { getCurrentUser } from "@/lib/actions/user";
+import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function SettingsPage() {
-  const user = await getCurrentUser();
+  const { user } = await getSession();
   if (!user) redirect("/login");
   return (
     <div className="mx-auto w-max">
-      <SettingsForm />
+      <div className="rounded-md border bg-destructive p-4 text-center text-destructive-foreground shadow">
+        <p className="text-lg">Bu sayfa henüz yapım aşamasında.</p>
+      </div>
     </div>
   );
 }
